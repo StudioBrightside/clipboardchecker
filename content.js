aad2b081da1529bd0e193b65b89045bbb46f4436
Joris W. van Rijn <hi@joriswvanrijn.com>
2021-01-21 10:41:36 +0100

    styling changes, fixes unexpected behavior with passwd manager

--- content.js
+++ content.js
@@ -5,10 +5,12 @@
 document.addEventListener("copy", async (event) => {
   let selection = document.getSelection();
 
-  chrome.runtime.sendMessage({
-    message: "check_clipboard",
-    selection: selection.toString(),
-  });
+  if (selection.toString() || 0 !== selection.toString().length) {
+    chrome.runtime.sendMessage({
+      message: "check_clipboard",
+      selection: selection.toString(),
+    });
+  }
 });
 
 /**
@@ -19,28 +21,9 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   // prevent the user from grave mistakes
   if (request.message === "show_notification") {
     // show popup
-    vNotify.error({
+    popup({
       title: "Wait up!",
-      text:
-        "Your clipboard might not contain what you expect. Check it before pasting.",
-      sticky: true,
-      showClose: true,
+      text: "It seems like this website has modified your clipboard. If you copied something, we advice you to check it before using it.",
     });
-
-    // highlight selection
-    // let node_name = document.getSelection().focusNode.parentNode.nodeName;
-
-    // if (node_name == "CODE") {
-    //   let range = document.getSelection().getRangeAt(0),
-    //     span = document.createElement("span");
-
-    //   span.className = "vnotify-highlight";
-    //   span.appendChild(range.extractContents());
-    //   range.insertNode(span);
-
-    //   span.addEventListener("click", function (e) {
-    //     e.target.classList.remove("vnotify-highlight");
-    //   });
-    // }
   }
 });
